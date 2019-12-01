515cb2f1ae9249899b514338c0713f57b52166a0
Jester0027 <Paul.NED@outlook.com>
2019-12-01 20:08:58 +0100

    :lipstick: Added the templates to the render - header and footer are in the render method

--- src/model/View.php
+++ src/model/View.php
@@ -10,20 +10,29 @@ class View
     private $title;
     private $request;
     private $session;
+    private $get;
 
     public function __construct()
     {
         $this->request = new Request();
         $this->session = $this->request->getSession();
+        $this->get = $this->request->getGet();
     }
 
     public function render($template, $data = [])
     {
         $this->file = './templates/' . $template . '.php';
+        $header = $this->renderFile('./templates/header.php', [
+            'get' => $this->get,
+            'session' => $this->session
+        ]);
         $content = $this->renderFile($this->file, $data);
+        $footer = $this->renderFile('./templates/footer.php', []);
         $view = $this->renderFile('./templates/base.php', [
             'title' => $this->title,
+            'header' => $header,
             'content' => $content,
+            'footer' => $footer,
             'session' => $this->session
         ]);
         echo $view;
