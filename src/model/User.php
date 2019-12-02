33335be0e5fa5a4ff36d0e935faab203b79c2172
Jester0027 <Paul.NED@outlook.com>
2019-12-02 18:20:45 +0100

    :card_file_box: Added user model

---
+++ src/model/User.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace BlogApp\src\model;
+
+class User
+{
+    private $id;
+    private $username;
+    private $role;
+    private $status;
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    public function setRole($role)
+    {
+        $this->role = $role;
+    }
+
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+}
