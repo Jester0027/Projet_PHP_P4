4c640164d310e302a44c6bae8d9aa3633281a8a5
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:20:40 +0100

    :card_file_box: Added DAOs in Controller - $articleDAO - $commentDAO - $userDAO

--- src/controller/Controller.php
+++ src/controller/Controller.php
@@ -4,9 +4,15 @@ namespace BlogApp\src\controller;
 
 use BlogApp\config\Request;
 use BlogApp\src\model\View;
+use BlogApp\src\DAO\ArticleDAO;
+use BlogApp\src\DAO\CommentDAO;
+use BlogApp\src\DAO\UserDAO;
 
 abstract class Controller
 {
+    protected $articleDAO;
+    protected $commentDAO;
+    protected $userDAO;
     protected $view;
     private $request;
     protected $get;
@@ -15,6 +21,9 @@ abstract class Controller
 
     public function __construct()
     {
+        $this->articleDAO = new ArticleDAO();
+        $this->commentDAO = new CommentDAO();
+        $this->userDAO = new UserDAO();
         $this->view = new View();
         $this->request = new Request();
         $this->get = $this->request->getGet();
