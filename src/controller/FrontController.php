7814197485958fd255dc83f93bc7b6ffe60ba8a2
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:27:33 +0100

    :recycle: Passing data in homepage and article - home page gets articles - article gets an article and its comments

--- src/controller/FrontController.php
+++ src/controller/FrontController.php
@@ -6,13 +6,19 @@ class FrontController extends Controller
 {
     public function getHome()
     {
-        return $this->view->render('home');
+        $articles = $this->articleDAO->getArticles();
+        return $this->view->render('home', [
+            'articles' => $articles
+        ]);
     }
 
-    public function getArticle()
+    public function getArticle($articleId)
     {
+        $article = $this->articleDAO->getArticle($articleId);
+        $comments = $this->commentDAO->getCommentsFromArticleId($articleId);
         return $this->view->render('article', [
-
+            'article' => $article,
+            'comments' => $comments
         ]);
     }
 }
