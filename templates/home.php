29d90efa1467d0a7a436c21bca9db4f814b14ce4
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:39:14 +0100

    :speech_balloon: Added homepage content

--- templates/home.php
+++ templates/home.php
@@ -1,3 +1,17 @@
 <?php $this->title = 'Accueil' ?>
 
-<p>Hello world</p>
+<div class="row">
+    <?php foreach($articles as $article) : ?>
+        <a href="./?route=article&articleId=<?= $article->getId(); ?>">
+            <div class="card indigo lighten-1 white-text">
+                <div class="card-content">
+                    <strong class="card-title"><?= htmlspecialchars($article->getTitle()); ?></strong>
+                    <p><?= $article->getContent(); ?></p>
+                </div>
+                <div class="card-action">
+                    Par : <?= htmlspecialchars($article->getAuthor()); ?>, le <?= htmlspecialchars($article->getCreatedAt()); ?>
+                </div>
+            </div>
+        </a>
+    <?php endforeach; ?>
+</div>
