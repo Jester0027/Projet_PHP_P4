893deb5a9b77c8431cc7fa2f0573442276f96385
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:36:49 +0100

    :speech_balloon: Added content in article view

--- templates/article.php
+++ templates/article.php
@@ -1,3 +1,27 @@
 <?php $this->title = 'Article' ?>
 
-<p>Article</p>
+<div class="row">
+    <h2><?= htmlspecialchars($article->getTitle()); ?></h2>
+    <div>
+        <?= $article->getContent(); ?>
+    </div>
+    <div class="row">
+        <p>Par : <?= htmlspecialchars($article->getAuthor()); ?></p>
+        <p>Le : <?= htmlspecialchars($article->getCreatedAt()); ?></p>
+    </div>
+</div>
+
+<div class="row">
+    <form action="./?route=addComment&articleId=<?= $article->getId(); ?>">
+        <label for="comment">Ajouter un commentaire</label>
+        <textarea name="comment" id="comment" cols="30" rows="10"></textarea>
+        <input type="submit" class="btn green" name="submit" value="Envoyer">
+    </form>
+    <h2>Commentaires</h2>
+
+    <?php foreach($comments as $comment) : ?>
+        <h6><?= htmlspecialchars($comment->getAuthor()); ?></h6>
+        <p>le <?= htmlspecialchars($comment->getCreatedAt()); ?></p>
+        <p><?= htmlspecialchars($comment->getContent()); ?></p>
+    <?php endforeach; ?>
+</div>
