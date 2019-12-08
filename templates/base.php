bf1deeaa34b3806d360926b26ddb99f3413d1519
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:37:13 +0100

    :heavy_plus_sign: Added tinyMCE

--- templates/base.php
+++ templates/base.php
@@ -6,6 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title><?= $title ?></title>
 
+    <script src="https://cdn.tiny.cloud/1/7oil4153ta5v1d7qubez5x7rha8a4qm6gjy18orgior3k9m0/tinymce/5/tinymce.min.js"></script>
+
+    <script>
+        tinymce.init({
+            selector: '.tinymce'
+        });
+    </script>
+
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
 
