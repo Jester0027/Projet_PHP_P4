e3a3b615a4271e9d7bee7a364f2f7b01e90a05b9
Jester0027 <Paul.NED@outlook.com>
2019-12-08 14:29:25 +0100

    :construction: Add article method - work in progress

--- src/controller/BackController.php
+++ src/controller/BackController.php
@@ -2,7 +2,15 @@
 
 namespace BlogApp\src\controller;
 
+use BlogApp\config\Parameter;
+
 class BackController extends Controller
 {
-
+    public function addArticle(Parameter $post)
+    {
+        if($post->get('submit')) {
+            
+        }
+        return $this->view->render('add_article');
+    }
 }
