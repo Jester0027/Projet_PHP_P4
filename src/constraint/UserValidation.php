56ca9a51f21f015bd9a07b78f628853994c5ddfa
Jester0027 <Paul.NED@outlook.com>
2019-12-09 12:36:19 +0100

    :lock: Validation + UserValidation classes - Validation : main class which checks validation - UserValidation : contains validations related to the user inputs

---
+++ src/constraint/UserValidation.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace BlogApp\src\constraint;
+
+use BlogApp\config\Parameter;
+
+class UserValidation extends Validation
+{
+
+    private $errors = [];
+    private $constraint;
+
+    public function __construct()
+    {
+        $this->constraint = new Constraint();
+    }
+
+    public function check(Parameter $post)
+    {
+        foreach ($post->all() as $key => $value) {
+            $this->checkField($key, $value);
+        }
+        return $this->errors;
+    }
+
+    private function checkField($name, $value)
+    {
+        if ($name === 'username') {
+            $error = $this->checkUsername($name, $value);
+            $this->addError($name, $error);
+        } else if ($name === 'password') {
+            $error = $this->checkPassword($name, $value);
+            $this->addError($name, $error);
+        } else if($name === 'email') {
+            $error = $this->checkEmail($name, $value);
+            $this->addError($name, $error);
+        }
+    }
+
+    private function addError($name, $error)
+    {
+        if ($error) {
+            $this->errors += [
+                $name => $error
+            ];
+        }
+    }
+
+    private function checkUsername($name, $value)
+    {
+        if ($this->constraint->notBlank($name, $value)) {
+            return $this->constraint->notBlank('pseudo', $value);
+        }
+        if ($this->constraint->minLength($name, $value, 2)) {
+            return $this->constraint->minLength('pseudo', $value, 2);
+        }
+        if ($this->constraint->maxLength($name, $value, 70)) {
+            return $this->constraint->maxLength('pseudo', $value, 70);
+        }
+    }
+
+    private function checkEmail($name, $value)
+    {
+        if ($this->constraint->notBlank($name, $value)) {
+            return $this->constraint->notBlank('E-mail', $value);
+        }
+        if ($this->constraint->minLength($name, $value, 2)) {
+            return $this->constraint->minLength('E-mail', $value, 2);
+        }
+        if ($this->constraint->maxLength($name, $value, 70)) {
+            return $this->constraint->maxLength('E-mail', $value, 70);
+        }
+    }
+
+    private function checkPassword($name, $value)
+    {
+        if ($this->constraint->notBlank($name, $value)) {
+            return $this->constraint->notBlank('mot de passe', $value);
+        }
+        if ($this->constraint->minLength($name, $value, 2)) {
+            return $this->constraint->minLength('mot de passe', $value, 2);
+        }
+        if ($this->constraint->maxLength($name, $value, 255)) {
+            return $this->constraint->maxLength('mot de passe', $value, 255);
+        }
+    }
+
+}
---
+++ src/constraint/Validation.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BlogApp\src\constraint;
+
+class Validation
+{
+    public function validate($data, $name)
+    {
+        if ($name === 'User') {
+            $userValidation = new UserValidation();
+            $errors = $userValidation->check($data);
+            return $errors;
+        }
+    }
+}
