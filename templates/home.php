8d693d78e5bb1278ed8477fcf4b891aeee656534
Jester0027 <Paul.NED@outlook.com>
2019-12-09 15:11:51 +0100

    :speech_balloon: Added notification messages - login - register - validation - logout

--- templates/home.php
+++ templates/home.php
@@ -1,8 +1,15 @@
 <?php $this->title = 'Accueil' ?>
 
+<p>
+    <?= $this->session->show('login') ?>
+    <?= $this->session->show('register') ?>
+    <?= $this->session->show('validation') ?>
+    <?= $this->session->show('logout') ?>
+</p>
+
 <div class="row">
-    <?php foreach($articles as $article) : ?>
-        <a href="./?route=article&articleId=<?= $article->getId(); ?>">
+    <?php foreach ($articles as $article) : ?>
+        <a href="./index.php?route=article&articleId=<?= $article->getId(); ?>">
             <div class="card indigo lighten-1 white-text">
                 <div class="card-content">
                     <strong class="card-title"><?= htmlspecialchars($article->getTitle()); ?></strong>
