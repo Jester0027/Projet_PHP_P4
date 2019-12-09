afbef66238d3e0f24e0e1f9cdc2029fa87592d1e
Jester0027 <Paul.NED@outlook.com>
2019-12-09 15:14:20 +0100

    :speech_balloon: Notification + dynamic login - if there's a problem while connection, an errors is shown - notified either if the account is not verified or for invalid inputs

--- templates/login.php
+++ templates/login.php
@@ -2,9 +2,14 @@
 
 <form method="POST" action="./index.php?route=login">
     <div class="row">
+        <div class="col m6 offset-m3 s12">
+            <p class="red-text"><?= $this->session->show('error_login') ?></p>
+        </div>
+    </div>
+    <div class="row">
         <div class="input-field col m6 offset-m3 s12">
-            <label for="username">Pseudo ou E-mail</label>
-            <input type="text" id="username" name="username">
+            <label for="username">Pseudo</label>
+            <input type="text" id="username" name="username" value="<?= isset($post) ? $post->get('username') : '' ?>">
         </div>
     </div>
 
