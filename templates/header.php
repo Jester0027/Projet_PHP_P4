65016b64d3b48cc1ea3857039217d888e01307f7
Jester0027 <Paul.NED@outlook.com>
2019-12-09 06:19:47 +0100

    :lipstick: Header update (login/register)

--- templates/header.php
+++ templates/header.php
@@ -6,6 +6,10 @@ $route = $get->get('route');
     <nav class="indigo">
         <div class="nav-wrapper container">
             <a href="./" class="brand-logo center">Jean Forteroche</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li class="<?= $route === 'login' ? 'active' : '' ?>"><a href="./?route=login">Se connecter</a></li>
+                <li class="<?= $route === 'register'  ? 'active' : '' ?>"><a href="./?route=register">S'inscrire</a></li>
+            </ul>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
                 <li class="<?= !$route ? 'active' : '' ?>"><a href="./">Accueil</a></li>
                 <li class="<?= $route === 'addArticle' ? 'active' : '' ?>"><a href="./?route=addArticle">Ajouter un article</a></li>
