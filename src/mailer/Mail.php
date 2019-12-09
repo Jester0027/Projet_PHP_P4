ba960b73d644c20807d8d024753d10f456a6f997
Jester0027 <Paul.NED@outlook.com>
2019-12-09 12:39:18 +0100

    :speech_balloon: Added content on sendConfirmation

--- src/mailer/Mail.php
+++ src/mailer/Mail.php
@@ -12,12 +12,18 @@ class Mail extends Mailer
         $to = $post->get('email');
         $subject = "Vérification d'authentification";
         $content = "
-            <h1>Bonjour " . $post->get('username') . "</h1>
+            <h1>Bonjour " . $post->get('username') . ",</h1>
+
+            <p>Vous avez demandé une inscription sur mon blog avec les identifiants suivants:</p>
+            <ul>
+                <li>Pseudo: " . $post->get('username') . "</li>
+                <li>Adresse E-mail: " . $post->get('email') . "</li>
+            </ul>
 
             <p>Il ne vous reste plus qu'une étape pour valider votre inscription</p>
-            <a href=\"" . $email . "\">Confirmer mon inscription</a>
+            <a href=\"" . $email . "\">Valider mon inscription</a>
         ";
 
-        $this->sendEmail($from, $to, $subject, $content);
+        $this->sendEmail($from, $to, $subject, $content, "Jean Forteroche");
     }
 }
