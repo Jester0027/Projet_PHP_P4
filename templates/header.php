e70115e12dd9619c3dd77340d2733dab4c25d9d5
Jester0027 <Paul.NED@outlook.com>
2019-12-09 15:10:42 +0100

    :children_crossing: Improved header - header is now dynamic - the content of the header depends on if the user is connected or not

--- templates/header.php
+++ templates/header.php
@@ -1,18 +1,29 @@
 <?php
 $route = $get->get('route');
 ?>
-
 <header>
     <nav class="indigo">
         <div class="nav-wrapper container">
-            <a href="./" class="brand-logo center">Jean Forteroche</a>
+            <a href="./index.php" class="brand-logo center">Jean Forteroche</a>
             <ul id="nav-mobile" class="left hide-on-med-and-down">
-                <li class="<?= $route === 'login' ? 'active' : '' ?>"><a href="./?route=login">Se connecter</a></li>
-                <li class="<?= $route === 'register'  ? 'active' : '' ?>"><a href="./?route=register">S'inscrire</a></li>
+                <?php if ($session->get('username')) {
+                    ?>
+                    <li class="<?= $route === 'logout' ? 'active' : '' ?>"><a href="./index.php?route=logout">Se déconnecter</a></li>
+                <?php
+                } else {
+                    ?>
+                    <li class="<?= $route === 'login' ? 'active' : '' ?>"><a href="./index.php?route=login">Se connecter</a></li>
+                    <li class="<?= $route === 'register'  ? 'active' : '' ?>"><a href="./index.php?route=register">S'inscrire</a></li>
+                <?php
+                } ?>
             </ul>
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li class="<?= !$route ? 'active' : '' ?>"><a href="./">Accueil</a></li>
-                <li class="<?= $route === 'addArticle' ? 'active' : '' ?>"><a href="./?route=addArticle">Ajouter un article</a></li>
+                <li class="<?= !$route ? 'active' : '' ?>"><a href="./index.php">Accueil</a></li>
+                <?php if ($session->get('role') === 'admin') {
+                    ?>
+                    <li class="<?= $route === 'admin' ? 'active' : '' ?>"><a href="./index.php?route=admin">Administration</a></li>
+                <?php
+                } ?>
             </ul>
         </div>
     </nav>
