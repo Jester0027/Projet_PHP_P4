1ee2fe0d866e0fd76f439e23851ae6528bb86dc5
Jester0027 <Paul.NED@outlook.com>
2019-12-09 06:14:31 +0100

    :card_file_box: Added registering and confirmation - register adds a new, not yet verified, user - confirmation sets the user to verified and removes the token

--- src/DAO/UserDAO.php
+++ src/DAO/UserDAO.php
@@ -6,17 +6,32 @@ use BlogApp\config\Parameter;
 
 class UserDAO extends DAO
 {
-    public function register(Parameter $post)
+    public function register(Parameter $post, $token)
     {
-        $sql = 'INSERT INTO User(username, password, role, status) VALUES(?, ?, 0, 0)';
+        $sql = 'INSERT INTO User(username, password, role, status, email, token) VALUES(?, ?, 0, 0, ?, ?)';
         $this->createQuery($sql, [
             $post->get('username'),
-            password_hash($post->get('password'), PASSWORD_BCRYPT)
+            password_hash($post->get('password'), PASSWORD_BCRYPT),
+            $post->get('email'),
+            $token
         ]);
     }
 
     public function login(Parameter $post)
     {
         
+    }
+
+    public function confirm(Parameter $get)
+    {
+        $token = $get->get('token');
+        $email = $get->get('email');
+        $sql = 'UPDATE user SET is_verified = ?, token = ? WHERE token = ? AND email = ?';
+        $this->createQuery($sql, [
+            1,
+            '',
+            $token,
+            $email
+        ]);
     }
 }
