2fbe7345985284424c587a55eccae549284ce3a7
Jester0027 <Paul.NED@outlook.com>
2019-12-09 12:38:27 +0100

    :bug: Mailer class fix

--- src/mailer/Mailer.php
+++ src/mailer/Mailer.php
@@ -3,18 +3,25 @@
 namespace BlogApp\src\mailer;
 
 use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
 abstract class Mailer
 {
     protected function sendEmail($from, $to, $subject, $content, $name ='')
     {
-        $mail = new PHPMailer();
-        $mail->setFrom($from);
-        $mail->addAddress($to, $name);
-        $mail->Subject = $subject;
-        $mail->isHTML(true);
-        $mail->body = $content;
-
-        $mail->send();
+        $mail = new PHPMailer(true);
+        try {
+            $mail->CharSet = 'UTF-8';
+            $mail->Encoding = 'base64';
+            $mail->setFrom($from, $name);
+            $mail->addAddress($to);
+            $mail->isHTML(true);
+            $mail->Subject = $subject;
+            $mail->Body = $content;
+    
+            $mail->send();
+        } catch(Exception $e) {
+            echo $e;
+        }
     }
 }
