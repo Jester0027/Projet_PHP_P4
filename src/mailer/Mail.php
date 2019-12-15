4de7a6285d1fb6902e2e1dbda0f85b84883fa67a
Jester0027 <Paul.NED@outlook.com>
2019-12-15 13:00:41 +0100

    :recycle: mail template rendering - sendConfirmation now renders a mail template

--- src/mailer/Mail.php
+++ src/mailer/Mail.php
@@ -6,23 +6,15 @@ use BlogApp\config\Parameter;
 
 class Mail extends Mailer
 {
-    public function sendConfirmation(Parameter $post, $email)
+    public function sendConfirmation(Parameter $post, $link)
     {
         $from = "jean.forteroche@blog.com";
         $to = $post->get('email');
         $subject = "Vérification d'authentification";
-        $content = "
-            <h1>Bonjour " . $post->get('username') . ",</h1>
-
-            <p>Vous avez demandé une inscription sur mon blog avec les identifiants suivants:</p>
-            <ul>
-                <li>Pseudo: " . $post->get('username') . "</li>
-                <li>Adresse E-mail: " . $post->get('email') . "</li>
-            </ul>
-
-            <p>Il ne vous reste plus qu'une étape pour valider votre inscription</p>
-            <a href=\"" . $email . "\">Valider mon inscription</a>
-        ";
+        $content = $this->render('confirm', [
+            'post' => $post,
+            'link' => $link
+        ]);
 
         return $this->sendEmail($from, $to, $subject, $content, "Jean Forteroche");
     }
