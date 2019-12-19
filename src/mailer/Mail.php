0452f7be0dae9498e5fbb9b1c150c2ea454c28de
Jester0027 <Paul.NED@outlook.com>
2019-12-19 15:04:47 +0100

    sendEmailChange method - sends a mail to the new address with the token

--- src/mailer/Mail.php
+++ src/mailer/Mail.php
@@ -4,6 +4,7 @@ namespace BlogApp\src\mailer;
 
 use BlogApp\config\IMail;
 use BlogApp\config\Parameter;
+use BlogApp\src\model\User;
 
 class Mail extends Mailer implements IMail
 {
@@ -28,6 +29,20 @@ class Mail extends Mailer implements IMail
         $content = $this->render('password_recovery', [
             'post' => $post,
             'link' => $link
+        ]);
+
+        return $this->sendEmail($from, $to, $subject, $content, self::NAME);
+    }
+
+    public function sendEmailChange(parameter $post, $link, User $user)
+    {
+        $from = self::ADDRESS;
+        $to = $post->get('newEmail');
+        $subject = "Changement d'adresse Email";
+        $content = $this->render('email_change', [
+            'post' => $post,
+            'link' => $link,
+            'user' => $user
         ]);
 
         return $this->sendEmail($from, $to, $subject, $content, self::NAME);
