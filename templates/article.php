ab053da688b6800a4018f6642314e89d71cdc6b3
Jester0027 <Paul.NED@outlook.com>
2019-12-19 10:07:53 +0100

    :lipstick: Article view update

--- templates/article.php
+++ templates/article.php
@@ -1,8 +1,13 @@
 <?php $this->title = htmlspecialchars($article->getTitle()); ?>
 
 <div class="row">
-    <h2><?= htmlspecialchars($article->getTitle()); ?></h2>
-    <hr>
+    <div class="col m6 offset-m3 s12">
+        <h2 class="center-align"><?= htmlspecialchars($article->getTitle()); ?></h2>
+        
+    </div>
+    <div class="col s12">
+        <hr>
+    </div>
     <div class="row">
         <div class="col s12">
             <?= $article->getContent(); ?>
@@ -15,16 +20,16 @@
 </div>
 
 <div class="row">
+    <h2>Commentaires</h2>
     <form method="POST" action="./index.php?route=addComment&articleId=<?= $article->getId(); ?>">
         <div class="input-field col s12">
             <label for="content">Ajouter un commentaire</label>
             <textarea class="materialize-textarea" name="content" id="content"></textarea>
         </div>
-        <div class="input-field col s12">
+        <div class="input-field col s12 right-align">
             <button type="submit" class="btn waves-effect waves-light green" name="submit">Envoyer</button>
         </div>
     </form>
-    <h2>Commentaires</h2>
 
     <?php foreach($comments as $comment) : ?>
         <div class="row">
