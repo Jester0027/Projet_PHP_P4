83b3d6d7cdee20401cf016543397ee04d85767e1
Jester0027 <Paul.NED@outlook.com>
2019-12-20 11:00:13 +0100

    :lock: User input changes - pw, username and email must now have minimum 4 chars

--- src/constraint/UserValidation.php
+++ src/constraint/UserValidation.php
@@ -4,7 +4,7 @@ namespace BlogApp\src\constraint;
 
 use BlogApp\config\Parameter;
 
-class UserValidation extends Validation
+class UserValidation
 {
 
     private $errors = [];
@@ -51,8 +51,8 @@ class UserValidation extends Validation
         if ($this->constraint->notBlank($name, $value)) {
             return $this->constraint->notBlank('pseudo', $value);
         }
-        if ($this->constraint->minLength($name, $value, 2)) {
-            return $this->constraint->minLength('pseudo', $value, 2);
+        if ($this->constraint->minLength($name, $value, 4)) {
+            return $this->constraint->minLength('pseudo', $value, 4);
         }
         if ($this->constraint->maxLength($name, $value, 70)) {
             return $this->constraint->maxLength('pseudo', $value, 70);
@@ -64,8 +64,8 @@ class UserValidation extends Validation
         if ($this->constraint->notBlank($name, $value)) {
             return $this->constraint->notBlank('E-mail', $value);
         }
-        if ($this->constraint->minLength($name, $value, 2)) {
-            return $this->constraint->minLength('E-mail', $value, 2);
+        if ($this->constraint->minLength($name, $value, 4)) {
+            return $this->constraint->minLength('E-mail', $value, 4);
         }
         if ($this->constraint->maxLength($name, $value, 70)) {
             return $this->constraint->maxLength('E-mail', $value, 70);
@@ -77,8 +77,8 @@ class UserValidation extends Validation
         if ($this->constraint->notBlank($name, $value)) {
             return $this->constraint->notBlank('mot de passe', $value);
         }
-        if ($this->constraint->minLength($name, $value, 2)) {
-            return $this->constraint->minLength('mot de passe', $value, 2);
+        if ($this->constraint->minLength($name, $value, 4)) {
+            return $this->constraint->minLength('mot de passe', $value, 4);
         }
         if ($this->constraint->maxLength($name, $value, 255)) {
             return $this->constraint->maxLength('mot de passe', $value, 255);
