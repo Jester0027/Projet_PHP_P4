0b1d6270207377e7550dc8c63191a2ec908c8d88
Jester0027 <Paul.NED@outlook.com>
2019-12-20 17:23:42 +0100

    :recycle: admin view refactor - each sections are now separated in files

---
+++ templates/templates/adminUsers.php
@@ -0,0 +1,40 @@
+<h3>Utilisateurs</h3>
+<p class="indigo-text">
+    <?= $this->session->show('user_action') ?>
+</p>
+<table>
+    <tr>
+        <th>ID</th>
+        <th>Pseudo</th>
+        <th>Role</th>
+        <th>Email</th>
+        <th>Status</th>
+        <th class="right-align">Actions</th>
+    </tr>
+    <?php
+    if($users) {
+        foreach ($users as $user) { ?>
+        <tr>
+            <td><?= $user->getId() ?></td>
+            <td><?= $user->getUsername() ?></td>
+            <td><?= $user->getRole() ?></td>
+            <td><?= $user->getEmail() ?></td>
+            <td><?= $user->getStatus() === '0' ? 'banni' : 'actif' ?></td>
+            <td class="right-align">
+                <?php if($user->getRole() === 'user') { ?>
+                    <a id="delete_user" href="index.php?route=deleteUser&userId=<?= $user->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">delete</i>Supprimer</a>
+                    <a id="ban_user" href="index.php?route=<?= $user->getStatus() === '1' ? 'banUser' : 'unbanUser' ?>&userId=<?= $user->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">gavel</i><?= $user->getStatus() === '0' ? 'Débannir' : 'Bannir' ?></a>
+                <?php } else { ?>
+                    <span class="grey-text">aucune action</span>
+                <?php } ?>
+            </td>
+        </tr>
+    <?php } 
+    } else { ?>
+        <tr>
+            <td colspan="5">
+                <p class="center-align">Aucun Utilisateur</p>
+            </td>
+        </tr>
+    <?php } ?>
+</table>
