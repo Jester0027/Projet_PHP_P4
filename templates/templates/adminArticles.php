b3bc9d1be18f2668cbd4873c2c85c5e0a24ea594
Jester0027 <Paul.NED@outlook.com>
2019-12-30 16:01:36 +0100

    :iphone: Responsive tables

--- templates/templates/adminArticles.php
+++ templates/templates/adminArticles.php
@@ -9,33 +9,37 @@
     <?= $this->session->show('user_action') ?>
     <span class="red-text"><?= $this->session->show('article_access') ?></span>
 </p>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Titre</th>
-        <th>Créé le</th>
-        <th>Par</th>
-        <th class="right-align">Actions</th>
-    </tr>
-    <?php
-    if($articles) {
-        foreach ($articles as $article) { ?>
+<table class="responsive-table">
+    <thead>
         <tr>
-            <td><?= $article->getId() ?></td>
-            <td><a href="index.php?route=article&articleId=<?= $article->getId() ?>"><?= $article->getTitle() ?></a></td>
-            <td><?= $article->getCreatedAt() ?></td>
-            <td><?= $article->getAuthor() ?></td>
-            <td class="right-align">
-                <a href="index.php?route=editArticle&articleId=<?= $article->getId() ?>" class="btn waves-effect waves-light green"><i class="material-icons left">edit</i>Modifier</a>
-                <a href="index.php?route=deleteArticle&articleId=<?= $article->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">delete</i>Supprimer</a>
-            </td>
+            <th>ID</th>
+            <th>Titre</th>
+            <th>Créé le</th>
+            <th>Par</th>
+            <th class="right-align">Actions</th>
         </tr>
-    <?php } 
-    } else { ?>
-        <tr>
-            <td colspan="5">
-                <p class="center-align">Aucun article</p>
-            </td>
-        </tr>
-    <?php } ?>
+    </thead>
+    <tbody>
+        <?php
+        if($articles) {
+            foreach ($articles as $article) { ?>
+            <tr>
+                <td><?= $article->getId() ?></td>
+                <td><a href="index.php?route=article&articleId=<?= $article->getId() ?>"><?= $article->getTitle() ?></a></td>
+                <td><?= $article->getCreatedAt() ?></td>
+                <td><?= $article->getAuthor() ?></td>
+                <td class="right-align">
+                    <a href="index.php?route=editArticle&articleId=<?= $article->getId() ?>" class="btn waves-effect waves-light green"><i class="material-icons left">edit</i>Modifier</a>
+                    <a href="index.php?route=deleteArticle&articleId=<?= $article->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">delete</i>Supprimer</a>
+                </td>
+            </tr>
+        <?php } 
+        } else { ?>
+            <tr>
+                <td colspan="5">
+                    <p class="center-align">Aucun article</p>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
 </table>
--- templates/templates/adminReports.php
+++ templates/templates/adminReports.php
@@ -1,30 +1,34 @@
 <h3>Commentaires signalés</h3>
-<table>
-    <tr>
-        <th>ID</th>
-        <th>Commentaire</th>
-        <th>Créé le</th>
-        <th>Par</th>
-        <th class="right-align">Actions</th>
-    </tr>
-    <?php if($reportedComments) { ?>
-        <?php foreach ($reportedComments as $reportedComment) { ?>
+<table class="responsive-table">
+    <thead>
         <tr>
-            <td><?= $reportedComment->getId() ?></td>
-            <td><?= $reportedComment->getContent() ?></td>
-            <td><?= $reportedComment->getCreatedAt() ?></td>
-            <td><?= $reportedComment->getAuthor() ?></td>
-            <td class="right-align">
-                <a id="delete_user" href="index.php?route=deleteComment&commentId=<?= $reportedComment->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">delete</i>Supprimer</a>
-                <a href="index.php?route=pardonComment&commentId=<?= $reportedComment->getId() ?>" class="btn waves-effect waves-light green">Désignaler</a>
-            </td>
+            <th>ID</th>
+            <th>Commentaire</th>
+            <th>Créé le</th>
+            <th>Par</th>
+            <th class="right-align">Actions</th>
         </tr>
-        <?php }
-    } else { ?>
-        <tr>
-            <td colspan="5">
-                <p class="center-align">Aucun commentaire signalé</p>
-            </td>
-        </tr>
-    <?php } ?>
+    </thead>
+    <tbody>
+        <?php if($reportedComments) { ?>
+            <?php foreach ($reportedComments as $reportedComment) { ?>
+            <tr>
+                <td><?= $reportedComment->getId() ?></td>
+                <td><?= $reportedComment->getContent() ?></td>
+                <td><?= $reportedComment->getCreatedAt() ?></td>
+                <td><?= $reportedComment->getAuthor() ?></td>
+                <td class="right-align">
+                    <a id="delete_user" href="index.php?route=deleteComment&commentId=<?= $reportedComment->getId() ?>" class="btn waves-effect waves-light red"><i class="material-icons left">delete</i>Supprimer</a>
+                    <a href="index.php?route=pardonComment&commentId=<?= $reportedComment->getId() ?>" class="btn waves-effect waves-light green">Désignaler</a>
+                </td>
+            </tr>
+            <?php }
+        } else { ?>
+            <tr>
+                <td colspan="5">
+                    <p class="center-align">Aucun commentaire signalé</p>
+                </td>
+            </tr>
+        <?php } ?>
+    </tbody>
 </table>
