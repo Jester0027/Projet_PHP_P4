656baa77525904ebb21ceb82938948b6b8eeb505
Jester0027 <Paul.NED@outlook.com>
2020-01-06 20:07:04 +0100

    :speech_balloon: Footer content

--- templates/footer.php
+++ templates/footer.php
@@ -2,16 +2,37 @@
     <div class="container">
         <div class="row">
             <div class="col l6 s12">
-                <h5 class="white-text">Footer Content</h5>
-                <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
+                <h5 class="white-text">Blog officiel de Jean Forteroche</h5>
             </div>
             <div class="col l4 offset-l2 s12">
-                <h5 class="white-text">Links</h5>
+                <h5 class="white-text">Réseaux sociaux</h5>
                 <ul>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                    <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="#!">
+                            <svg class="left" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;">
+                                <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                    <path d="M0,172v-172h172v172z" fill="none"></path>
+                                    <g fill="#ffffff">
+                                        <path d="M136.16667,21.5h-100.33333c-7.91917,0 -14.33333,6.41417 -14.33333,14.33333v100.33333c0,7.91917 6.41417,14.33333 14.33333,14.33333h54.61717v-49.88717h-16.7915v-19.52917h16.7915v-14.36917c0,-16.65533 10.18383,-25.7355 25.0475,-25.7355c5.0095,-0.01433 10.01183,0.24367 14.99267,0.7525v17.415h-10.234c-8.09833,0 -9.675,3.827 -9.675,9.47433v12.43417h19.35l-2.5155,19.52917h-16.94917v49.91583h25.69967c7.91917,0 14.33333,-6.41417 14.33333,-14.33333v-100.33333c0,-7.91917 -6.41417,-14.33333 -14.33333,-14.33333z"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                            Facebook
+                        </a>
+                    </li>
+                    <li>
+                        <a class="grey-text text-lighten-3" href="#!">
+                            <svg class="left" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" width="24" height="24" viewBox="0 0 172 172" style=" fill:#000000;">
+                                <g fill="none" fill-rule="nonzero" stroke="none" stroke-width="1" stroke-linecap="butt" stroke-linejoin="miter" stroke-miterlimit="10" stroke-dasharray="" stroke-dashoffset="0" font-family="none" font-weight="none" font-size="none" text-anchor="none" style="mix-blend-mode: normal">
+                                    <path d="M0,172v-172h172v172z" fill="none"></path>
+                                    <g fill="#ffffff">
+                                        <path d="M136.16667,21.5h-100.33333c-7.91917,0 -14.33333,6.41417 -14.33333,14.33333v100.33333c0,7.91917 6.41417,14.33333 14.33333,14.33333h100.33333c7.91917,0 14.33333,-6.41417 14.33333,-14.33333v-100.33333c0,-7.91917 -6.41417,-14.33333 -14.33333,-14.33333zM122.19167,68.18367c0,0.61633 0,1.2255 0,2.45817c0,23.34183 -17.81633,50.3745 -50.3745,50.3745c-9.8255,0 -19.04183,-3.0745 -27.0255,-7.98367c1.2255,0 3.0745,0 4.3,0c7.98367,0 15.9745,-3.0745 22.11633,-7.3745c-7.98367,0 -14.1255,-5.5255 -16.58367,-12.28367c1.2255,0 2.45817,0.61633 3.0745,0.61633c1.84183,0 3.0745,0 4.91633,-0.61633c-7.98367,-1.84183 -14.1255,-8.6 -14.1255,-17.2c2.45817,1.2255 4.91633,1.84183 7.98367,2.45817c-4.91633,-4.3 -7.98367,-9.21633 -7.98367,-15.35817c0,-3.0745 0.61633,-6.14183 2.45817,-8.6c8.6,10.44183 21.5,17.81633 36.24183,18.4255c0,-1.2255 -0.61633,-2.45817 -0.61633,-4.3c0,-9.8255 7.98367,-17.81633 17.81633,-17.81633c4.91633,0 9.8255,1.84183 12.9,5.5255c4.3,-0.61633 7.98367,-2.45817 11.05817,-4.3c-1.2255,4.3 -4.3,7.3745 -7.98367,9.8255c3.68367,-0.61633 6.75817,-1.2255 10.44183,-3.0745c-2.4725,3.698 -5.53983,6.76533 -8.61433,9.2235z"></path>
+                                    </g>
+                                </g>
+                            </svg>
+                            Twitter
+                        </a>
+                    </li>
                 </ul>
             </div>
         </div>
